f4a58c8d2838c6798c01ed6b3114eb3539919ca1
JohnSamuel <yoe.johnson@gmail.com>
2019-11-04 12:08:39 +0530

    added path

--- tap-parser.js
+++ tap-parser.js
@@ -35,7 +35,7 @@ var p = new Parser(function (results) {
         message: JSON.stringify(message),
         path: path
     });
-    textAnnotations.push(`##[error] ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} `)
+    textAnnotations.push(`##[error] ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} path ${path} `)
    })
    //console.log(annotations);
    console.log(textAnnotations);
