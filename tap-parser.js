2410cba2b46a715263fa8d532317fe0a48567338
JohnSamuel <yoe.johnson@gmail.com>
2019-11-05 12:11:22 +0530

    hol

--- tap-parser.js
+++ tap-parser.js
@@ -35,6 +35,7 @@ var p = new Parser(function (results) {
         message: JSON.stringify(message),
         path: path
     });
+    
     textAnnotations.push(`##[error] ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} path:${path} name:${name}`)
    })
    //console.log(annotations);
