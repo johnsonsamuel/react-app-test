05221322e1c56b4b93c3e8edbcd38b6b83ea9e71
JohnSamuel <yoe.johnson@gmail.com>
2019-11-05 13:58:22 +0530

    return errors

--- tap-parser.js
+++ tap-parser.js
@@ -4,65 +4,44 @@ const annotations = [];
 const textAnnotations = [];
 
 var p = new Parser(function (results) {
-   //console.dir(JSON.stringify(results).replace(/'/g, ""));
+    //console.dir(JSON.stringify(results).replace(/'/g, ""));
 
-   JSON.parse(JSON.stringify(results).replace(/'/g, "")).failures.map(item => {
-       //console.log(item.diag)
-       if(!item.diag) return;
-       const splitItem = item.diag.at.split('js:');
-      
-      const fileName = splitItem[0];
-      const name = item.diag.name;
-      const message = item.diag.message || item.diag.values;
-      
-      //const lineNumbersArr = splitItem[1];
-      //console.log(splitItem[1].replace(")",""), '====')
-      //console.log(JSON.parse(lineNumbers));
-     const lineNumbers = splitItem[1].replace(")","").split(':');
-        
-      //const lineNumbers = lineNumbersArr;
-      
-      const path = fileName.split('(')[1]+'js';
-    //   console.log(path);
-      //console.log(lineNumbers);
-      annotations.push({
-        start_line: parseInt(lineNumbers[0]),
-        end_line: parseInt(lineNumbers[0]),
-        start_column: 0,
-        end_column: parseInt(lineNumbers[1]),
-        annotation_level: 'failure',
-        title: name,
-        message: JSON.stringify(message),
-        path: path
+    JSON.parse(JSON.stringify(results).replace(/'/g, "")).failures.map(item => {
+        //console.log(item.diag)
+        if (!item.diag) return;
+        const splitItem = item.diag.at.split('js:');
+
+        const fileName = splitItem[0];
+        const name = item.diag.name;
+        const message = item.diag.message || item.diag.values;
+
+        //const lineNumbersArr = splitItem[1];
+        //console.log(splitItem[1].replace(")",""), '====')
+        //console.log(JSON.parse(lineNumbers));
+        const lineNumbers = splitItem[1].replace(")", "").split(':');
+
+        //const lineNumbers = lineNumbersArr;
+
+        const path = fileName.split('(')[1] + 'js';
+        //   console.log(path);
+        //console.log(lineNumbers);
+        annotations.push({
+            start_line: parseInt(lineNumbers[0]),
+            end_line: parseInt(lineNumbers[0]),
+            start_column: 0,
+            end_column: parseInt(lineNumbers[1]),
+            annotation_level: 'failure',
+            title: name,
+            message: JSON.stringify(message),
+            path: path
+        });
+
+        textAnnotations.push(`##[error] ${splitItem[1].replace(")", "")} error ${JSON.stringify(message)} path:${path} name:${name}`)
     });
-    
-    textAnnotations.push(`##[error] ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} path:${path} name:${name}`)
-   })
-   //console.log(annotations);
-   console.log(textAnnotations);
-   //throw textAnnotations;
 
-//    try {
-//     var errors = [];
-//     for (let i=0; i<textAnnotations.length; i++) {
-//       errors.push(new Error(textAnnotations[i]));
-//     }
-  
-//     if (errors.length > 0) {
-//       throw errors; 
-//     }
-  
-//   } catch(e) {
-//       for (let i=0; i<e.length; i++) {
-//         console.log(e[i]);
-//       }
-//   }
-   
-//    textAnnotations.map(error => {
-//         throw Error(error);
-//    });
-   /*throw [ '##[error] 3:1 error "test is not defined" ',
-   '##[error] 4:4 error "Test failed via `t.fail()`" ' ]; */
+    const errors = textAnnotations.map(error => new Error(error));
+
+    console.log(errors);
 });
 
 
