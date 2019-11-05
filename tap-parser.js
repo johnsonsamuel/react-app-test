ef8e9753d7f6cd89ef36b2d1e2c02de5e8d1c399
JohnSamuel <yoe.johnson@gmail.com>
2019-11-05 15:19:48 +0530

    throw errors

--- tap-parser.js
+++ tap-parser.js
@@ -24,7 +24,7 @@ var p = new Parser(function (results) {
       
       const path = fileName.split('(')[1]+'js';
     //   console.log(path);
-      //console.log(lineNumbers);
+      console.log(lineNumbers);
       annotations.push({
         start_line: parseInt(lineNumbers[0]),
         end_line: parseInt(lineNumbers[0]),
@@ -35,13 +35,10 @@ var p = new Parser(function (results) {
         message: JSON.stringify(message),
         path: path
     });
-    textAnnotations.push(`##[error] ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} ${path} path:${path}`)
+    textAnnotations.push(`##error ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} `)
    })
    //console.log(annotations);
    console.log(textAnnotations);
-   //throw Error(textAnnotations);
-//    throw [ '##[error] 3:1 error "test is not defined" ',
-//    '##[error] 4:4 error "Test failed via `t.fail()`" ' ]; 
 });
 
 
