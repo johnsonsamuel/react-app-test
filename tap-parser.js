c038307041727761e88a20d4364b4bb369fab941
JohnSamuel <yoe.johnson@gmail.com>
2019-11-13 21:15:37 +0530

    updated tap parser to be in sync with check run

--- tap-parser.js
+++ tap-parser.js
@@ -38,8 +38,8 @@ const p = new Parser(function (results) {
         path: path
     });
 
-    //checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")","")} error ${message} path:${path} name:${name}`);
-    checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")","")} file=${path}, line=${parseInt(lineNumbers[0])} ,col=0::${message}`)
+    checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")","")} error ${message} path:${path} name:${name}`);
+    //checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")","")} file=${path}, line=${parseInt(lineNumbers[0])} ,col=0::${message}`)
    });
 
    checkApiAnnotationsFormat.push(testSummary);
