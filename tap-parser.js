d8704300f8f55450a1548a76fc99662a9522e323
JohnSamuel <yoe.johnson@gmail.com>
2019-11-13 21:25:16 +0530

    updated tap parser to be in sync with check run

--- tap-parser.js
+++ tap-parser.js
@@ -42,7 +42,7 @@ const p = new Parser(function (results) {
     //checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")","")} file=${path}, line=${parseInt(lineNumbers[0])} ,col=0::${message}`)
    });
 
-   checkApiAnnotationsFormat.push(`echo "::warning file=test.spec.js,line=4,col=0::Missing semicolon"`)
+   checkApiAnnotationsFormat.push(`##[warning] file=test.spec.js,line=4,col=0::Missing semicolon"`)
 
    checkApiAnnotationsFormat.push(testSummary);
    console.log(checkApiAnnotationsFormat); //FIXME: Printing the error as per the check run format
