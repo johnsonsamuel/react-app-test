0c3825b69557070275bc813d236cac9baaf8f7f9
JohnSamuel <yoe.johnson@gmail.com>
2019-11-14 17:40:36 +0530

    test..

--- tap-parser.js
+++ tap-parser.js
@@ -1,50 +1,38 @@
 const Parser = require('tap-parser');
 const annotations = [];
 
-const checkApiAnnotationsFormat = []; //annotations to feed as per github check api...
+const checkApiAnnotationsFormat = []; //annotations to feed as per github check api....
 
 const p = new Parser(function (results) {
-    
-   //Adding the unit test count.... 
-   const testOutput = JSON.parse(JSON.stringify(results));
-   
-   const testSummary = {
+
+    //Adding the unit test count.... 
+    const testOutput = JSON.parse(JSON.stringify(results));
+
+    const testSummary = {
         total: testOutput.count,
         failure: testOutput.fail,
         success: testOutput.pass
-   };
+    };
 
-   const stringifiedResult =  JSON.stringify(results).trim(); 
-   const parseResult = JSON.parse(stringifiedResult.replace(/'/g, ""));
+    const stringifiedResult = JSON.stringify(results).trim();
+    const parseResult = JSON.parse(stringifiedResult.replace(/'/g, ""));
 
-   //Iterate through the failures and add annotations 
-   parseResult.failures.map(item => {
-       if(!item.diag) return;
-       const splitItem = item.diag.at.split('js:');
-      
-      const fileName = splitItem[0];
-      const name = item.diag.name;
-      const message = item.diag.message || item.diag.values;
-    //   const lineNumbers = splitItem[1].replace(")","").split(':');         
-      const path = fileName.split('(')[1]+'js';
+    //Iterate through the failures and add annotations 
+    parseResult.failures.map(item => {
+        if (!item.diag) return;
+        const splitItem = item.diag.at.split('js:');
 
-      /* annotations.push({
-        start_line: parseInt(lineNumbers[0]),
-        end_line: parseInt(lineNumbers[0]),
-        start_column: 0,
-        end_column: parseInt(lineNumbers[1]),
-        annotation_level: 'warning',
-        title: name,
-        message: JSON.stringify(message),
-        path: path
-    }); */
+        const fileName = splitItem[0];
+        const name = item.diag.name;
+        const message = item.diag.message || item.diag.values;
+        const path = fileName.split('(')[1] + 'js';
 
-    checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")","")} error ${JSON.stringify(message)} path:${path} name:${name}`);
-   });
+        checkApiAnnotationsFormat.push(`##[error] ${splitItem[1].replace(")", "")} error ${JSON.stringify(message)} path:${path} name:${name}`);
+    });
 
-   checkApiAnnotationsFormat.push(testSummary);
-   //checkApiAnnotationsFormat.push(`##[debug] ${JSON.stringify(testSummary)} `);
-   console.log(checkApiAnnotationsFormat); //FIXME: Printing the error as per the check run format
+    checkApiAnnotationsFormat.push(testSummary);
+    //checkApiAnnotationsFormat.push(`##[debug] ${JSON.stringify(testSummary)} `);
+    console.log(checkApiAnnotationsFormat); //FIXME: Printing the error as per the check run format
 });
 
 
