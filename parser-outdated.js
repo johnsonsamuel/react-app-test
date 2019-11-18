c5c266ae0e62a2ee24bec7de5bafcf9343b6ed95
JohnSamuel <yoe.johnson@gmail.com>
2019-11-18 12:48:53 +0530

    parser

--- parser-outdated.js
+++ parser-outdated.js
@@ -12,15 +12,16 @@ stdin.on('data', function (chunk) {
 
 stdin.on('end', function () {
     const outputJSON = JSON.parse(inputChunks);
+   // console.log(outputJSON)
     Object.values(outputJSON).map(item => {
 
-        annotationsFormat.push(`##[warning] current:${(JSON.stringify(item))}`);
+        annotationsFormat.push(`##[warning] current:${outputJSON}`);
     });
     
     //stdout.write(JSON.stringify(outputJSON));
     //stdout.write('\n');
    /*  stdout.write(JSON.stringify(annotationsFormat));
     stdout.write('\n'); */
-    console.log(annotationsFormat)
+    console.log(outputJSON)
 });
 
