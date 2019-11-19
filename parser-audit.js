8a55cde20c5739d1b93fb641400dadbcfbade96c
JohnSamuel <yoe.johnson@gmail.com>
2019-11-19 11:22:19 +0530

    add parser audit

---
+++ parser-audit.js
@@ -0,0 +1,18 @@
+var stdin = process.stdin,
+    stdout = process.stdout,
+    inputChunks = [],
+    annotationsFormat = [];
+
+stdin.resume();
+stdin.setEncoding('utf8');
+
+stdin.on('data', function (chunk) {
+    inputChunks.push(chunk);
+});
+
+stdin.on('end', function () {
+    if(!inputChunks.length) return;
+    //const outputJSON = JSON.parse(inputChunks);
+    console.log('##[warning]~'+(inputChunks));
+});
+
