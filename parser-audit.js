dfa8c60c8a27f131c7d1d438938745210349c1fd
JohnSamuel <yoe.johnson@gmail.com>
2019-11-19 11:53:23 +0530

    add parser audit

--- parser-audit.js
+++ parser-audit.js
@@ -12,17 +12,7 @@ stdin.on('data', function (chunk) {
 
 stdin.on('end', function () {
     if(!inputChunks.length) return;
-    console.log(`##[warning]~ # Run  npm install mongoose@5.7.11  to resolve 1 vulnerability
-    ┌───────────────┬──────────────────────────────────────────────────────────────┐
-    │ High          │ Denial of Service                                            │
-    ├───────────────┼──────────────────────────────────────────────────────────────┤
-    │ Package       │ mongodb                                                      │
-    ├───────────────┼──────────────────────────────────────────────────────────────┤
-    │ Dependency of │ mongoose                                                     │
-    ├───────────────┼──────────────────────────────────────────────────────────────┤
-    │ Path          │ mongoose > mongodb                                           │
-    ├───────────────┼──────────────────────────────────────────────────────────────┤
-    │ More info     │ https://nodesecurity.io/advisories/1203                      │
-    └───────────────┴──────────────────────────────────────────────────────────────┘ `);
+    //console.log('='+inputChunks)
+    console.log('##[warning]~'+inputChunks);
 });
 
